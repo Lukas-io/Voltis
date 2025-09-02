4f504d8c77ba15c39c77d728c8b8f54756fff2fd
Lukas-io <wisdomiyamu@gmail.com>
2025-09-02 18:15:12 +0100

    fix git issues

--- Voltis/ContentView.swift
+++ Voltis/ContentView.swift
@@ -8,26 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-<<<<<<< HEAD
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
-
-=======
 
     var body: some View {
         ProfileView()
 
     }
 }
->>>>>>> d13eb98 (Initial Commit)
 #Preview {
     ContentView()
 }
